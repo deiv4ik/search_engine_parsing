22760731b9d6c989b9b2627026b2dea43447a45a
deiv4ik <is_davlat@mail.ru>
2023-03-09 18:23:13 +0500

    Parsing

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <set>
 #include <string>
 #include <vector>
 
@@ -25,14 +26,36 @@ vector<string> SplitIntoWords(const string& text) {
     return words;
 }
 
-string ReadSearchQuery() {
-        string query;
-        getline(cin, query);
-        return query;
+set<string> ParseStopWords(const string& text) {
+    set<string> stop_words;
+    for (const string& word : SplitIntoWords(text)) {
+        stop_words.insert(word);
+    }
+    return stop_words;
+}
+
+vector<string> ParseQuery(const string& text, const set<string>& stop_words) {
+    vector<string> words;
+    for (const string& word : SplitIntoWords(text)) {
+        if (stop_words.count(word) == 0) {
+            words.push_back(word);
+        }
+    }
+    return words;
 }
 
 int main() {
-      for (const auto& x : SplitIntoWords(ReadSearchQuery())) {
-          cout << '[' << x << ']' << " ";
-      }
+    // Read stop words
+    string stop_words_joined;
+    getline(cin, stop_words_joined);
+    const set<string> stop_words = ParseStopWords(stop_words_joined);
+
+    // Read query
+    string query;
+    getline(cin, query);
+    const vector<string> query_words = ParseQuery(query, stop_words);
+
+    for (const string& word : query_words) {
+        cout << '[' << word << ']' << endl;
+    }
 }
